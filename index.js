43e8f8368a3859e8cda294f18e1a05c7822626c5
Abhishek Raj <abhishek.raj@nagarro.com>
2018-05-16 23:10:56 +0530

    push

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ Developed by Abhishek Raj on 15-05-2018
 
 */
 
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
+
+var cors = require('cors')
+
 const {
     Student
 } = require("./db/db");
 
 const app = express();
+app.use(cors());
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({
